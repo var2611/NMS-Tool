da4b61c6117c6e6b745dd22d11627a603376a6cb
Vardhan Suthar <var2611@gmail.com>
2026-06-11 11:17:53 +0000

    chore: bump version to v2.2.1

--- core/config.py
+++ core/config.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
     # App
     app_mode: str = Field("desktop", env="APP_MODE")
     app_name: str = "SentinelNMS"
-    app_version: str = "2.2.0"
+    app_version: str = "2.2.1"
     debug: bool = Field(False, env="DEBUG")
 
     # API
